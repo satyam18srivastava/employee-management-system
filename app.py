6771302286625f3b98e15cffabaafbca4bdc7497
Satyam Srivastava <satyamsrivastava1968@gmail.com>
2026-02-10 00:09:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # LOGIN PAGE
 @app.route('/')
