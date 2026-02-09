d5c1bf438eb18edbc6694483c52be4b50f525634
Satyam Srivastava <satyamsrivastava1968@gmail.com>
2026-02-09 23:40:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# LOGIN PAGE
 @app.route('/')
-def home():
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username == "admin" and password == "1234":
+            return "Login Successful"
+        else:
+            return "Invalid Username or Password"
+
     return render_template('login.html')
 
-@app.route('/signup')
+
+# SIGNUP PAGE
+@app.route('/signup', methods=['GET','POST'])
 def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return "User Registered Successfully"
+
     return render_template('signup.html')
 
-@app.route('/forgot')
+
+# FORGOT PASSWORD PAGE
+@app.route('/forgot', methods=['GET','POST'])
 def forgot():
+    if request.method == 'POST':
+        email = request.form['email']
+        return "Password reset link sent"
+
     return render_template('forgot.html')
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+    app.run(debug=True)
