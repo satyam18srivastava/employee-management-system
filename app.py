00bf023422dbd9c4f29749e42a3ccb854f34bd0e
Satyam <satyamsrivastava1968@gmail.com>
2026-02-15 15:33:08 +0530

    fix render deploy

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def signup():
 def forgot():
     return render_template('forgot.html')
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
